10cf99b0da9a7fbf1f7984340802fd50d1ea35a9
vvtachev <vvtachev@gmail.com>
2021-05-13 22:24:43 +0300

    training commit

---
+++ app/src/main/java/org/example/vvtachev/flickrbrowser/FlickrRecyclerViewAdapter.java
@@ -0,0 +1,36 @@
+package org.example.vvtachev.flickrbrowser;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+class FlickrRecyclerViewAdapter extends RecyclerView.Adapter<FlickrRecyclerViewAdapter.FlickrImageViewHolder> {
+    private static final String TAG = "FlickrRecyclerViewAdapt";
+    private List<Photo> photosList;
+    private Context context;
+
+    public FlickrRecyclerViewAdapter(List<Photo> photosList, Context context) {
+        this.photosList = photosList;
+        this.context = context;
+    }
+
+    static class FlickrImageViewHolder extends RecyclerView.ViewHolder {
+        private static final String TAG = "FlickrImageViewHolder";
+        ImageView thumbnail = null;
+        TextView title = null;
+
+        public FlickrImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Log.d(TAG, "FlickrImageViewHolder: starts");
+            this.thumbnail = itemView.findViewById(R.id.thumbnail);
+            this.title = itemView.findViewById(R.id.title);
+        }
+    }
+}
